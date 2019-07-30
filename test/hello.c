781ff3de9924881f098d16f7b4ca90738c5ec0ef
luhuadong <luhuadong@163.com>
2019-07-30 19:40:53 +0800

    增加helloworld测试程序

---
+++ test/hello.c
@@ -0,0 +1,19 @@
+/*================================================================
+*   Copyright (C) 2019 Guangzhou Firefly Ltd. All rights reserved.
+*   
+*   文件名称：hello.c
+*   创 建 者：luhuadong
+*   创建日期：2019年07月30日
+*   描    述：
+*
+================================================================*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
