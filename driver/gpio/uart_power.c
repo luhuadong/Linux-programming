0a2bc13905ca8378e2302c1ab0cecfa159739857
luhuadong <luhuadong@163.com>
2021-03-04 01:35:22 +0800

    update uart_power driver

--- driver/gpio/uart_power.c
+++ driver/gpio/uart_power.c
@@ -25,20 +25,20 @@
 #include <linux/platform_device.h>
 
 // #define USER_LED      374       /*GPIO LED  GPIO4_22*/
-#define USER_LED       321       /*UART_PWR_EN  GPIO5_01*/
+#define UART_PWR_EN      321       /*UART_PWR_EN  GPIO5_01*/
 
 int major;
-struct class *myled_class;
-struct device *myled_class_devs;
+struct class  *uart_power_class;
+struct device *uart_power_class_devs;
 
 
-static int led_chr_dev_open(struct inode *inode, struct file *filp)
+static int uart_power_chrdev_open(struct inode *inode, struct file *filp)
 {
     printk("\n open form driver \n");
     return 0;
 }
 
-static ssize_t led_chr_dev_write(struct file *filp, const char __user *buf, size_t cnt, loff_t *offt)
+static ssize_t uart_power_chrdev_write(struct file *filp, const char __user *buf, size_t cnt, loff_t *offt)
 {
     unsigned char write_data; //用于保存接收到的数据
 
@@ -52,14 +52,14 @@ static ssize_t led_chr_dev_write(struct file *filp, const char __user *buf, size
     return 0;
 }
 
-static struct file_operations myled_drv_fops =
+static struct file_operations uart_power_drv_fops =
 {
     .owner = THIS_MODULE,
-    .open = led_chr_dev_open,
-    .write = led_chr_dev_write,
+    .open = uart_power_chrdev_open,
+    .write = uart_power_chrdev_write,
 };
 
-static long myled_drv_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static long uart_power_drv_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {   
     printk("gpio_ioctl\n");
     switch(cmd) {
@@ -67,13 +67,13 @@ static long myled_drv_ioctl(struct file *file, unsigned int cmd, unsigned long a
         case 1:
             if(arg == 0)
             {
-                gpio_set_value(USER_LED, 0);
-                printk("led is off\n");
+                gpio_set_value(UART_PWR_EN, 0);
+                printk("uart power off\n");
                 break;
             }
             else {
-                gpio_set_value(USER_LED, 1);
-                printk("led is on\n");
+                gpio_set_value(UART_PWR_EN, 1);
+                printk("uart power on\n");
                 break;
             }
         default:
@@ -82,63 +82,64 @@ static long myled_drv_ioctl(struct file *file, unsigned int cmd, unsigned long a
     return 0;
 }
 
-static  int myled_probe(struct platform_device *pdev)
+static  int uart_power_probe(struct platform_device *pdev)
 {
     int ret;
-    ret = gpio_request(USER_LED, "LED");//第一个参数，为要申请的引脚，第二个为你要定义的名字
+    ret = gpio_request(UART_PWR_EN, "UART_PWR_EN");//第一个参数，为要申请的引脚，第二个为你要定义的名字
+    
     if (ret) 
     {
         printk("[pual] gpio_request error %s - %d -- \n",__func__,__LINE__);
-                return ret;
-       }
-    gpio_direction_output(USER_LED, 1);
-    gpio_set_value(USER_LED, 1);
-    major = register_chrdev(0,"myled",&myled_drv_fops);
+        return ret;
+    }
+
+    gpio_direction_output(UART_PWR_EN, 1);
+    gpio_set_value(UART_PWR_EN, 1);
+    major = register_chrdev(0, "uart-power", &uart_power_drv_fops);
     
-      //创建设备信息，执行后会出现 /sys/class/myled
-      myled_class = class_create(THIS_MODULE, "myled");
+    //创建设备信息，执行后会出现 /sys/class/uart-power
+    uart_power_class = class_create(THIS_MODULE, "uart-power");
 
     //创建设备节点，就是根据上面的设备信息来的
-      myled_class_devs = device_create(myled_class, NULL, MKDEV(major, 0), NULL, "myled"); /* /dev/myled */
+    uart_power_class_devs = device_create(uart_power_class, NULL, MKDEV(major, 0), NULL, "uart-power"); /* /dev/uart-power */
     
     return 0;   
 }
 
-static const struct of_device_id myled_ids[] = {
+static const struct of_device_id uart_power_ids[] = {
     
     { .compatible = "gpio-uart-power", },
     { },
 };
+//MODULE_DEVICE_TABLE(of, uart_power_ids);
 
-static struct platform_driver myled_driver={
-    .probe  = myled_probe,
-    //.remove = myled_remove,
+static struct platform_driver uart_power_driver={
+    
     .driver = {
-        .name  = "myled",
-        .of_match_table = myled_ids,
-    }
+        .name  = "uart_power",
+        .owner = THIS_MODULE,
+        .of_match_table = uart_power_ids,
+    },
+    .probe  = uart_power_probe,
+    //.remove = uart_power_remove,
 };
+//module_platform_driver(uart_power_driver); /* call module_init() */
 
-static int __init myled_init(void)
+static int __init uart_power_init(void)
 {
     /*2. 注册平台驱动*/
-    platform_driver_register(&myled_driver);
+    platform_driver_register(&uart_power_driver);
     return 0;
 }
 
-static void __exit myled_exit(void)
+static void __exit uart_power_exit(void)
 {
-
     /*3. 注销平台驱动*/
-    platform_driver_unregister(&myled_driver);
-    
+    platform_driver_unregister(&uart_power_driver);
 }
 
-module_init(myled_init);
-module_exit(myled_exit);
-
-//module_init(uart_power_platform_driver_init);
-//module_exit(uart_power_platform_driver_exit);
+module_init(uart_power_init);
+module_exit(uart_power_exit);
 
 MODULE_AUTHOR("Rudy Lo <luhuadong@163.com>");
 MODULE_LICENSE("GPL v2");
